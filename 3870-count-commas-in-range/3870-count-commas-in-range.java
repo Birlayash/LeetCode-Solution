61abebce40d53815334b70634623ed2af7977e00
Yash Birla <150151749+Birlayash@users.noreply.github.com>
2026-09-08 13:14:13 +0530

    Time: 1 ms (83.65%), Space: 42.4 MB (72.85%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,15 +1,8 @@
 class Solution {
     public int countCommas(int n) {
-        if(n<1000){
-            return 0;
+        if(n>=1000){
+            return n-999;
         }
-        int count=0;
-        
-        while(n>=1000){
-            count++;
-            n=n-1;
-            
-        }
-        return count;
+        return 0;
     }
 }
